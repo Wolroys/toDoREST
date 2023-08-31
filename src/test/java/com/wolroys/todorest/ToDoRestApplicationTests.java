aedd1302659c5be43ae58c92b948e870bcba43ef
Wolroys <alekseypaslenov@gmail.com>
2023-08-31 22:12:42 +0300

    git init

---
+++ src/test/java/com/wolroys/todorest/ToDoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolroys.todorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
